4b839a3104d4247dc641f8d489442aab1cf8c48c
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-03-15 10:45:00 +0300

    slider swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-$(function () {});
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
